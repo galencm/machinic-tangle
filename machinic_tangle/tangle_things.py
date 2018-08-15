c9b00742f2268a334715c9de558cc565c0fffebe
Galen Curwen-McAdams <galencm@fastmail.com>
2018-08-15 20:51:30 +0000

    Problem: cannot generate code for peripherals
    useful for adding additional facets to the
    running system. Using gsl for code generation
    allows for a variety of outputs: a button model
    could create a software gui button or hardware
    code to run  on a wireless device. Inexpensive
    chips such as the esp82666, available toolchains
    such as platformio core, frameworks like homie
    and message brokers make reconfigurable hardware
    things quickly useful. Derived from earlier
    machinic projects such as ma and the concept of
    projectional systems.
    
    Solution: define tangle-things as an entrypoint
    script to find models(.xml) and templates(.gsl),
    specify any initial configuration and generate.
    
    Create a basic button model.
    
    Create a template that generates a button that runs
    using the homie framework, structured to use platformio
    core to handle installing dependencies, compiling and
    uploading.

---
+++ machinic_tangle/tangle_things.py
@@ -0,0 +1,60 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2018, Galen Curwen-McAdams
+
+import pathlib
+import os
+import shutil
+import argparse
+import uuid
+import subprocess
+from lxml import etree
+
+# code generation approaches derived from earlier machinic
+# approaches, see ma and the concept of projectional systems
+
+def scaffold_thing(thing_name, thing_type, model_type):
+    # create an initial directory to hold whatever is generated
+    cwd = os.path.join(os.getcwd(), "{}_{}".format(thing_type, thing_name))
+    if not os.path.isdir(cwd):
+        os.mkdir(cwd)
+
+    model_file = "model_{}.xml".format(thing_type)
+    # update model xml with thing name
+    model_xml = etree.parse(str(pathlib.PurePath(module_path(), model_file)))
+    for peripheral in model_xml.xpath('//peripheral'):
+        peripheral.set("name", thing_name)
+        for output in peripheral.xpath('.//output'):
+            # prefer publish nodes?
+            output.set("channel", "/{}".format(thing_name))
+            # value setting is a default for testing button
+            # will have to take into account things such as 
+            # sensors that might send varying values
+            output.set("value", "1".format(thing_name))
+
+    model_xml.write(str(pathlib.PurePath(cwd, model_file)), pretty_print=True)
+    template_file = "iot_{}_{}.gsl".format(model_type, thing_type)
+    shutil.copy(pathlib.PurePath(module_path(), template_file), cwd)
+    # run gsl to generate code / create directories
+    subprocess.call(["gsl","-script:{}".format(template_file), model_file], cwd=cwd)
+    subprocess.call(["chmod", "+x", "regenerate.sh"], cwd=cwd)
+    subprocess.call(["./regenerate.sh"], cwd=cwd)
+
+def module_path():
+    return pathlib.PurePath(pathlib.Path(__file__).parents[0])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("thing_type", help="type of thing")
+    parser.add_argument("--model-type", default="homie", help="model to use")
+    parser.add_argument("--name", default=None, help="thing name (default is a uuid)")
+
+    args = parser.parse_args()
+
+    if not args.name:
+        args.name = str(uuid.uuid4())
+
+    args = vars(args)
+    scaffold_thing(args["name"], args["thing_type"], args["model_type"])
