24202af3780c492b9599fbde78207fe425be0115
Galen Curwen-McAdams <galencm@fastmail.com>
2018-08-21 07:10:47 +0000

    Problem: lack dsl to route between broker and db
    Solution: create pathling, inspired by routeling
    to handle conditional routing, setting keys, and
    shell calls. Can be moved to machinic-lings
    package in future.

---
+++ machinic_tangle/pathling_validate_spec.py
@@ -0,0 +1,28 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2018, Ga
+
+import os
+import pathlib
+from textx.metamodel import metamodel_from_file
+
+
+pathling_model_file = pathlib.Path(pathlib.PurePath(pathlib.Path(__file__).parents[0], "pathling.tx"))
+pathling_metamodel = metamodel_from_file(pathling_model_file)
+passed = 0
+failed = 0
+
+with open("pathling_spec.txt", "r") as f:
+    for line in f.read().split("\n"):
+        if not line.startswith("#") and line:
+            try:
+                path = pathling_metamodel.model_from_str(line)
+                print("PASSED:{}\n".format(line))
+                passed += 1
+            except Exception as ex:
+                print("FAILED:\n{}\n{}\n".format(line, ex))
+                failed += 1
+
+print("passed: {}\nfailed: {}".format(passed, failed))
