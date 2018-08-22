7cabd6a4adae78f718b465ba713088b1ddbb52ce
Galen Curwen-McAdams <galencm@fastmail.com>
2018-08-22 17:56:22 +0000

    Problem: truncated license header
    Solution: correct

--- machinic_tangle/pathling_validate_spec.py
+++ machinic_tangle/pathling_validate_spec.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2018, Ga
+# Copyright (c) 2018, Galen Curwen-McAdams
 
 import os
 import pathlib
