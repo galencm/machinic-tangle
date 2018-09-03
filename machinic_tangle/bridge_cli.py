32976ad51da324aebd3d1a9cc84c72d8460f556b
Galen Curwen-McAdams <galencm@fastmail.com>
2018-09-03 12:21:32 +0000

    Problem: bridge started via cli has no env vars
    Solution: include some basic env vars unless
    passed --no-basic-env-vars flag.

--- machinic_tangle/bridge_cli.py
+++ machinic_tangle/bridge_cli.py
@@ -16,9 +16,24 @@ def main():
     parser.add_argument("--broker-host",  default="127.0.0.1", help="broker host ip")
     parser.add_argument("--broker-port", type=int, default=1883, help="broker port")
     parser.add_argument("--allow-shell-calls", action="store_true", help="allow ling shell calls")
+    parser.add_argument("--no-basic-env-vars", action="store_true", help="basic DB_* and BROKER_* env_vars")
     parser.add_argument("--verbose", action="store_true", help="")
     args = parser.parse_args()
-    print(args.db_host, args.db_port, args.broker_host, args.broker_port)
-    b = bridge.Bridge(args.db_host, args.db_port, args.broker_host, args.broker_port, allow_shell_calls=args.allow_shell_calls)
+    # usually env vars a passed in by program
+    # that imports bridge such as tangle-ui
+    # include some basics by default
+    # env_vars is called as a function, so pass a lambda
+    if args.no_basic_env_vars is True:
+        env_vars = None
+    else:
+        env_vars = lambda: {
+        "$DB_HOST" : args.db_host,
+        "$DB_PORT" : args.db_port,
+        "$BROKER_HOST" : args.broker_host,
+        "$BROKER_PORT" : args.broker_port
+                    }
+
+    print(args.db_host, args.db_port, args.broker_host, args.broker_port, env_vars)
+    b = bridge.Bridge(args.db_host, args.db_port, args.broker_host, args.broker_port, allow_shell_calls=args.allow_shell_calls, env_vars=env_vars)
     while True:
         time.sleep(0.1)
